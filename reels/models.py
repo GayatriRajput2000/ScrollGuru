f0ab63261317cb638e44abf44ad0229a24150715
Gayatri Rajput <gayatrising.rajput@gmail.com>
2026-03-29 00:01:18 +0530

    create reel

--- reels/models.py
+++ reels/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Reel(models.Model):
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reels"
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    video = models.FileField(upload_to="reels/videos/")
+    thumbnail = models.ImageField(upload_to="reels/thumbnails/", null=True, blank=True)
+
+    category = models.CharField(max_length=50)
+
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- reels/views.py
+++ reels/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Reel
+from .serializers import ReelSerializer
 
-# Create your views here.
+
+class ReelListView(generics.ListAPIView):
+    queryset = Reel.objects.all().order_by("-created_at")
+    serializer_class = ReelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ReelCreateView(generics.CreateAPIView):
+    queryset = Reel.objects.all()
+    serializer_class = ReelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+        
